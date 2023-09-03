e9fe72ba7721accf5d9cab7a3cd4955aa1234da6
Татьяна Сердюк <131401889+TatyanaSerdyuk@users.noreply.github.com>
2023-09-03 19:10:43 +0700

    AbsPageObject.java

--- src/main/java/pageObject/AbsPageObject.java
+++ src/main/java/pageObject/AbsPageObject.java
@@ -1,2 +1,20 @@
-package pageObject;public class AbsPageObject {
+package pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class AbsPageObject {
+    protected WebDriver driver;
+    protected Actions actions;
+    protected WebDriverWait wait;
+
+    public AbsPageObject(WebDriver driver){
+
+        this.driver = driver;
+        this.actions = new Actions(driver);
+        this.wait = new WebDriverWait(driver, 15);
+        PageFactory.initElements(driver, this);
+    }
 }
