4069519abf2f1731fbb778ae9cf75940310751ce
Татьяна Сердюк <131401889+TatyanaSerdyuk@users.noreply.github.com>
2023-09-03 19:08:33 +0700

    CoursesData.java

--- src/main/java/data/CoursesData.java
+++ src/main/java/data/CoursesData.java
@@ -1,2 +1,17 @@
-package data;public class CoursesData {
+package data;
+
+public enum CoursesData {
+    TESTING("Тестирование"),
+    PROGRAMMING("Программирование");
+
+  private String name;
+
+    CoursesData(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
