38a54d76f609f1aa52595375567f8061a24ab257
Татьяна Сердюк <131401889+TatyanaSerdyuk@users.noreply.github.com>
2023-09-03 19:11:13 +0700

    BasePage.java

--- src/main/java/pageObject/BasePage.java
+++ src/main/java/pageObject/BasePage.java
@@ -1,2 +1,21 @@
-package pageObject;public class BasePage {
+package pageObject;
+
+import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.WebDriver;
+
+
+public abstract class BasePage extends AbsPageObject{
+
+    private final static String BASE_URL = System.getProperty("base.url");
+
+    public BasePage(WebDriver driver){
+        super(driver);
+    }
+
+    protected org.apache.logging.log4j.Logger logger = LogManager.getLogger(BasePage.class);
+
+
+    public void open(String path) {
+        driver.get(BASE_URL + path);
+    }
 }
